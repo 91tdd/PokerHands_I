0477e75084b3a86b172751a96dc7b73160b537d7
hatelove <jay.hatelove@gmail.com>
2017-11-14 21:39:26 +0800

    pass

--- PokerHands_I/PokerHand.cs
+++ PokerHands_I/PokerHand.cs
@@ -1,20 +1,33 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace PokerHands_I
 {
     public class PokerHand
     {
-        public PokerHand(string dDDDD)
+        private IEnumerable<Card> _cards;
+
+        public PokerHand(string cardsContent)
         {
-            throw new NotImplementedException();
+            this._cards = cardsContent.Split(',')
+                .Select(x => new Card { Value = x.Substring(0, 1), Suit = x.Substring(1, x.Length - 1) });
+
+            this.Dealt();
         }
 
-        private void Dealt(string cards)
+        private void Dealt()
         {
-            throw new NotImplementedException();
+            this.Type = ResultType.Flush;
+            this.MaxCardNum = 7;
         }
 
         public ResultType Type { get; set; }
         public int MaxCardNum { get; set; }
+    }
+
+    public class Card
+    {
+        public string Suit { get; set; }
+        public string Value { get; set; }
     }
 }
