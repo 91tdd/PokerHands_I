6a6eb52943c3edc17dca080ffd0115cdcc11234d
hatelove <jay.hatelove@gmail.com>
2017-11-15 00:01:53 +0800

    refactor testing

--- PokerHands_I/PokerHandComparerTest.cs
+++ PokerHands_I/PokerHandComparerTest.cs
@@ -5,13 +5,19 @@ namespace PokerHands_I
     [TestClass]
     public class PokerHandGameTest
     {
+        private PokerHandGame pokerHandGame = new PokerHandGame("Black", "White");
+
         [TestMethod]
         public void HighCard_Compare()
         {
-            PokerHandGame pokerHandGame = new PokerHandGame("Black", "White");
-            pokerHandGame.Play("2D,3S,4H,5S,8S", "3D,2S,6H,KS,8H");
+            OutputShouldBe("2D,3S,4H,5S,8S", "3D,2S,6H,KS,8H", "White Win. -with high card: King");
+        }
+
+        private void OutputShouldBe(string firstPlayerCards, string secondPlayerCards, string expected)
+        {
+            pokerHandGame.Play(firstPlayerCards, secondPlayerCards);
             string actual = pokerHandGame.Output;
-            Assert.AreEqual("White Win. -with high card: King", actual);
+            Assert.AreEqual(expected, actual);
         }
     }
 }
