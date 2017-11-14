fa641a2e37bd6800fcdf4f57645f30843e77e5f5
hatelove <jay.hatelove@gmail.com>
2017-11-14 22:45:09 +0800

    pass

--- PokerHands_I/ThreeOfAKindHandler.cs
+++ PokerHands_I/ThreeOfAKindHandler.cs
@@ -1,24 +1,28 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace PokerHands_I
 {
     internal class ThreeOfAKindHandler : IPokerHandler
     {
         private readonly PokerHand _pokerHand;
+        private IEnumerable<IGrouping<short, Card>> _cardsGroupByValue;
 
         public ThreeOfAKindHandler(PokerHand pokerHand)
         {
             _pokerHand = pokerHand;
+            _cardsGroupByValue = _pokerHand._cards.GroupBy(x => x.Value);
         }
 
         public bool IsMatch()
         {
-            throw new NotImplementedException();
+            return _cardsGroupByValue.Any(x => x.Count() == 3);
         }
 
         public void SetResult()
         {
-            throw new NotImplementedException();
+            _pokerHand.Type = ResultType.ThreeOfAKind;
+            _pokerHand.MaxCardNum = _cardsGroupByValue.First(x => x.Count() == 3).Key;
         }
     }
 }
